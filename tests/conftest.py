69fd51e757744c5416dea26864e0112efe902321
Andrey <volk000@gmail.com>
2023-02-17 10:13:41 +0600

    Add print DATABASE_URL

--- tests/conftest.py
+++ tests/conftest.py
@@ -14,6 +14,7 @@ def get_test_db():
     if os.path.isfile(TEST_ENV_FILE):
         load_dotenv(TEST_ENV_FILE, override=True)
     DATABASE_URL = os.getenv('DATABASE_URL')
+    print(DATABASE_URL)
     connection = psycopg2.connect(DATABASE_URL)
 
     with connection as conn:
