0c580175158a3a0714c0c7d26265aff76d879846
Andrey <volk000@gmail.com>
2023-02-17 10:46:36 +0600

    Fix TEST_DATABASE_URL

--- tests/conftest.py
+++ tests/conftest.py
@@ -7,14 +7,13 @@ from psycopg2.extras import NamedTupleCursor
 
 @pytest.fixture()
 def get_test_db():
-    TEST_ENV_FILE = "tests/.env"
     with open("database.sql", "r") as file:
         db_creation_commands = file.read()
 
     if not os.getenv('TEST_DATABASE_URL'):
-        load_dotenv(TEST_ENV_FILE, override=True)
-    DATABASE_URL = os.getenv('TEST_DATABASE_URL')
-    connection = psycopg2.connect(DATABASE_URL)
+        load_dotenv("tests/.env", override=True)
+    TEST_DATABASE_URL = os.getenv('TEST_DATABASE_URL')
+    connection = psycopg2.connect(TEST_DATABASE_URL)
 
     with connection as conn:
         with conn.cursor(cursor_factory=NamedTupleCursor) as curs:
