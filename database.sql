1a6fb1f8fa3ad8a4de919b0e18ca6dbec8fcd12e
Andrey <volk000@gmail.com>
2023-03-13 23:54:41 +0600

    Fix syntax in database.sql

--- database.sql
+++ database.sql
@@ -1,16 +1,15 @@
+CREATE TABLE urls(
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    name varchar(255) NOT NULL UNIQUE,
+    created_at timestamp NOT NULL
+);
 
-    CREATE TABLE urls(
-        id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-        name varchar(255) NOT NULL UNIQUE,
-        created_at timestamp NOT NULL
-    )
-
-    CREATE TABLE checks(
-        id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
-        url_id bigint REFERENCES urls(id),
-        status_code smallint,
-        h1 text,
-        title text,
-        description text,
-        created_at timestamp NOT NULL
-    );
+CREATE TABLE checks(
+    id bigint PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+    url_id bigint REFERENCES urls(id),
+    status_code smallint,
+    h1 text,
+    title text,
+    description text,
+    created_at timestamp NOT NULL
+);
